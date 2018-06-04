2f5b893d0c5bdeae761057725796e082bee4dc19
Daniel Pinheiro <danielfpinheiro98@gmail.com>
2018-06-04 00:22:17 -0300

    Implementação de Período

---
+++ BLL/Entidades/Periodo.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL.Estruturas;
+
+namespace BLL.Entidades
+{
+    class Periodo : IEntidade, IDado
+    {
+        private int numero;
+
+        public int Numero { get { return this.numero; } }
+
+        public Periodo(int numero)
+        {
+            this.numero = numero;
+        }
+
+        public int CompareTo(IDado other)
+        {
+            if (this.numero < ((Periodo)other).Numero)
+                return -1;
+            else if (this.numero > ((Periodo)other).Numero)
+                return 1;
+            else
+                return 0;
+        }
+
+        public bool Equals(IDado other)
+        {
+            if (this.Equals((Horario)other)) //cast para lançar exceção
+                return true;
+            else
+                return false;
+        }
+
+        public bool ExisteNoBanco()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void RemoverDoBanco()
+        {
+            throw new NotSupportedException();
+        }
+
+        public void SalvarNoBanco()
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
