767f91fe121e49900f7b3d39a4123452ecb0ac2e
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-06-10 16:43:16 -0300

    Leitura de arquivos funcionando!
    Basta chamar a LeituraArquivo (estática) e o método LerArquivo(string nomeArquivo, char caractereSeparador) e recuperar o retorno do método, que é uma lista de vetores de string, na qual cada posição contém os dados de uma linha do arquivo. Ou seja, o length da lista será igual à quantidade de linhas do arquivo.

---
+++ DAL/LeituraArquivo/LeituraArquivo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DAL.LeituraArquivo
+{
+    public static class LeituraArquivo
+    {
+        static private List<string[]> matrizDados;
+
+        static string nomeArquivo;
+        static StreamReader read;
+
+        static LeituraArquivo()
+        {
+            matrizDados = new List<string[]>();
+        }
+
+        /// <summary>
+        /// Retorna uma matriz, sendo que cada linha possui as informações de uma
+        /// linha do arquivo.
+        /// </summary>
+        /// <param name="nomeArq">Nome do arquivo que será retornado em forma de vetor.</param>
+        /// <param name="caractereSeparador">Caractere que separa as informações de uma
+        /// mesma linha no arquivo.</param>
+        /// <returns></returns>
+        static public List<string[]> LerAquivo(string nomeArq, char caractereSeparador)
+        {
+            string[] conteudoArq;
+
+            nomeArquivo = nomeArq;
+
+            if (nomeArquivo.IndexOf(".txt") == -1)
+            {
+                nomeArquivo = nomeArquivo + ".txt";
+            }
+
+            read = new StreamReader(nomeArquivo);
+
+            string texto = read.ReadToEnd();
+
+            texto = texto.Replace("\r", "");
+
+            conteudoArq = texto.Split('\n');
+
+            PreencherMatriz(conteudoArq, caractereSeparador);
+
+            read.Close();
+
+            return matrizDados;
+        }
+
+        static private void PreencherMatriz(string[] transfereDados, char caractereSeparador)
+        {
+            string[] vetorAux;
+            int quantColunas = transfereDados[0].Split(caractereSeparador).Length;
+            int quantLinhas = transfereDados.Length;
+
+            for (int i = 0; i < quantLinhas; i++)
+            {
+                vetorAux = transfereDados[i].Split(caractereSeparador);
+                matrizDados.Add(vetorAux);
+            }
+        }
+    }
+}
