085f731cb3f003ac2c455d218e767881770a0a99
Daniel Pinheiro <danielfpinheiro98@gmail.com>
2018-10-27 12:28:52 -0300

    Criação da UTL (Unit Test Layer) + Auto stash
    Com exemplo de AbrirConexao

---
+++ UTL/BLL/ManagerTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using UTL.Excecoes;
+
+namespace UTL.BLL
+{
+    [TestClass]
+    public class ManagerTest
+    {
+        [TestMethod]
+        public void AbrirConexao()
+        {
+            string msgErro;
+
+            Assert.AreEqual(true, Manager.AbrirConexao(out msgErro), msgErro);
+        }
+    }
+}
---
+++ UTL/Excecoes/ErroConexaoException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTL.Excecoes
+{
+    class ErroConexaoException : Exception
+    {
+        public ErroConexaoException() : base("Não foi possível realizar a conexão.") { }
+        public ErroConexaoException(string msg) : base(msg) { }
+    }
+}
