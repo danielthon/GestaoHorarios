575846a084cf1f7749d1408a72d6ba45fb087813
Pedro H. Ferreira Fonseca <pedro.henriqueff10@gmail.com>
2018-11-29 07:58:17 -0200

    Começando testes de integração. Esboço de cada método está pronto!

--- UTL/TestesIntegracao/TestEntidades.cs
+++ UTL/TestesIntegracao/TestEntidades.cs
@@ -27,8 +27,12 @@ namespace UTL.TestesIntegracao
             // feche a conexão e drop o banco aqui
         }
 
+        /// <summary>
+        /// Tentar salvar um administrador na tabela "Administrador".
+        /// </summary>
         [TestMethod]
-        public void SalvarNoBancoAdministrador()
+        [TestProperty("Description", "Tentar salvar um administrador na tabela \"Administrador\".")]
+        public void TI_AD_001()
         {
             Administrador adm = new Administrador("AdministradorTeste", "1234");
             string msgAcao;
@@ -47,8 +51,210 @@ namespace UTL.TestesIntegracao
             Assert.AreEqual(true, adm.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
         }
 
+        /// <summary>
+        /// Tentar salvar um administrador na tabela "Administrador".
+        /// </summary>
         [TestMethod]
-        public void SalvarNoBancoAlocacao()
+        [TestProperty("Description", "Tentar alterar um administrador na tabela \"Administrador\".")]
+        public void TI_AD_002()
+        {
+            Administrador adm = new Administrador("AdministradorTeste", "1234");
+            string msgAcao;
+
+            if (adm.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            adm.SalvarNoBanco();
+
+            Assert.AreEqual(true, adm.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar salvar um administrador na tabela "Administrador".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar remover um administrador na tabela \"Administrador\".")]
+        public void TI_AD_003()
+        {
+            Administrador adm = new Administrador("AdministradorTeste", "1234");
+            string msgAcao;
+
+            if (adm.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            adm.SalvarNoBanco();
+
+            Assert.AreEqual(true, adm.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar salvar um professor na tabela "Professor".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar salvar um professor na tabela \"Professor\".")]
+        public void TI_PR_001()
+        {
+            Professor prof = new Professor("ProfessorTester", "1234");
+
+            string msgAcao;
+
+            if (prof.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            prof.SalvarNoBanco();
+
+            Assert.AreEqual(true, prof.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar alterar um professor na tabela "Professor".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar alterar um professor na tabela \"Professor\".")]
+        public void TI_PR_002()
+        {
+            Professor prof = new Professor("ProfessorTester", "1234");
+
+            string msgAcao;
+
+            if (prof.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            prof.SalvarNoBanco();
+
+            Assert.AreEqual(true, prof.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar remover um professor na tabela "Professor".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar remover um professor na tabela \"Professor\".")]
+        public void TI_PR_003()
+        {
+            Professor prof = new Professor("ProfessorTester", "1234");
+
+            string msgAcao;
+
+            if (prof.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            prof.SalvarNoBanco();
+
+            Assert.AreEqual(true, prof.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar salvar uma disciplina na tabela "Disciplina".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar salvar uma disciplina na tabela \"Disciplina\".")]
+        public void TI_DI_001()
+        {
+            Disciplina discip = new Disciplina("DisciplinaTester", 0, 0);
+
+            string msgAcao;
+
+            if (discip.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            discip.SalvarNoBanco();
+
+            Assert.AreEqual(true, discip.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar alterar uma disciplina na tabela "Disciplina".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar alterar uma disciplina na tabela \"Disciplina\".")]
+        public void TI_DI_002()
+        {
+            Disciplina discip = new Disciplina("DisciplinaTester", 0, 0);
+
+            string msgAcao;
+
+            if (discip.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            discip.SalvarNoBanco();
+
+            Assert.AreEqual(true, discip.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar remover uma disciplina na tabela "Disciplina".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar remover uma disciplina na tabela \"Disciplina\".")]
+        public void TI_DI_003()
+        {
+            Disciplina discip = new Disciplina("DisciplinaTester", 0, 0);
+
+            string msgAcao;
+
+            if (discip.ExisteNoBanco())
+            {
+                msgAcao = "atualizado";
+            }
+            else
+            {
+                msgAcao = "inserido";
+            }
+
+            discip.SalvarNoBanco();
+
+            Assert.AreEqual(true, discip.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+        }
+
+        /// <summary>
+        /// Tentar salvar uma alocação na tabela "Alocação".
+        /// </summary>
+        [TestMethod]
+        [TestProperty("Description", "Tentar salvar uma alocação na tabela \"Alocacao\".")]
+        public void TI_AL_001()
         {
             Disciplina discip = new Disciplina("DisciplinaTeste", 0, 0);
             Horario hor = new Horario(DiaSemana.Segunda, Hora._19h00);
@@ -71,47 +277,60 @@ namespace UTL.TestesIntegracao
             Assert.AreEqual(true, aloc.ExisteNoBanco(), "A alocação não foi " + msgAcao + " corretamente no banco");
         }
 
+        /// <summary>
+        /// Tentar alterar uma alocação na tabela "Alocação".
+        /// </summary>
         [TestMethod]
-        public void SalvarNoBancoDisciplina()
+        [TestProperty("Description", "Tentar alterar uma alocação na tabela \"Alocacao\".")]
+        public void TI_AL_002()
         {
-            Disciplina discip = new Disciplina("DisciplinaTester", 0, 0);
+            Disciplina discip = new Disciplina("DisciplinaTeste", 0, 0);
+            Horario hor = new Horario(DiaSemana.Segunda, Hora._19h00);
+
+            Alocacao aloc = new Alocacao(discip, hor);
 
             string msgAcao;
 
-            if (discip.ExisteNoBanco())
+            if (aloc.ExisteNoBanco())
             {
-                msgAcao = "atualizado";
+                msgAcao = "atualizada";
             }
             else
             {
-                msgAcao = "inserido";
+                msgAcao = "inserida";
             }
 
-            discip.SalvarNoBanco();
+            aloc.SalvarNoBanco();
 
-            Assert.AreEqual(true, discip.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+            Assert.AreEqual(true, aloc.ExisteNoBanco(), "A alocação não foi " + msgAcao + " corretamente no banco");
         }
 
+        /// <summary>
+        /// Tentar remover uma alocação na tabela "Alocação".
+        /// </summary>
         [TestMethod]
-        public void SalvarNoBancoProfessor()
+        [TestProperty("Description", "Tentar remover uma alocação na tabela \"Alocacao\".")]
+        public void TI_AL_003()
         {
-            Professor prof = new Professor("ProfessorTester", "1234");
+            Disciplina discip = new Disciplina("DisciplinaTeste", 0, 0);
+            Horario hor = new Horario(DiaSemana.Segunda, Hora._19h00);
+
+            Alocacao aloc = new Alocacao(discip, hor);
 
             string msgAcao;
 
-            if (prof.ExisteNoBanco())
+            if (aloc.ExisteNoBanco())
             {
-                msgAcao = "atualizado";
+                msgAcao = "atualizada";
             }
             else
             {
-                msgAcao = "inserido";
+                msgAcao = "inserida";
             }
 
-            prof.SalvarNoBanco();
+            aloc.SalvarNoBanco();
 
-            Assert.AreEqual(true, prof.ExisteNoBanco(), "O usuário não foi " + msgAcao + " corretamente no banco");
+            Assert.AreEqual(true, aloc.ExisteNoBanco(), "A alocação não foi " + msgAcao + " corretamente no banco");
         }
-
     }
 }
